edbbedbbc1754551912612e2a90bd740bb430c99
Simon Prosser <simon@pagelines.com>
2013-03-20 20:05:43 +0000

    Fixes
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- hooker.php
+++ hooker.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Hooker
 Description: Easily add any code anywhere using all the built in hooks with a simple gui.
-Version: 1.2
+Version: 1.3
 Author: Simon Prosser
-Demo: 
+Demo:
 Author URI: http://pross.org.uk
 PageLines: true
 */
@@ -24,11 +24,11 @@ class PLHooks {
 
 		if ( ! function_exists( 'ploption' ) )
 			return;
-		if ( !current_user_can('edit_theme_options') )
+		if ( ! current_user_can( 'edit_theme_options' ) )
 			return;
 
 		global $hooks_menu;
-		$hooks_menu = pagelines_insert_menu( PL_MAIN_DASH, 'Hook Editor', 'edit_theme_options', 'pagelines_hooks', array( &$this, 'get_admin_page') );
+		$hooks_menu = pagelines_insert_menu( PL_MAIN_DASH, 'Hook Editor', 'edit_theme_options', 'pagelines_hooks', array( &$this, 'get_admin_page' ) );
 
 		add_action( 'load-' . $hooks_menu, 'pagelines_theme_settings_scripts' );
 		add_action( 'load-' . $hooks_menu, array( &$this, 'save_settings' ) );
@@ -39,9 +39,9 @@ class PLHooks {
 
 		global $options;
 		global $wp_admin_bar;
-		if ( current_user_can('edit_theme_options') ) {
+		if ( current_user_can( 'edit_theme_options' ) && is_object( $wp_admin_bar ) ) {
 
-			$wp_admin_bar->add_menu( array( 'id' => 'pl_hooks', 'parent' => 'pl_settings', 'title' => __('Hooker', 'pagelines'), 'href' => admin_url( 'admin.php?page=pagelines_hooks' ) ) );
+			$wp_admin_bar->add_menu( array( 'id' => 'pl_hooks', 'parent' => 'pl_settings', 'title' => __( 'Hooker', 'pagelines' ), 'href' => admin_url( 'admin.php?page=pagelines_hooks' ) ) );
 		}
 
 		$options = get_option( 'pl_hooks_editor', array() );
@@ -62,17 +62,17 @@ class PLHooks {
 
 					foreach( $page as $o => $id )
 						if( $id == $post->ID ) {
-							add_action( $data['hook'], create_function( '$hook', "PLHooks::run_action($hook);"), $data['priority'] );
+							add_action( $data['hook'], create_function( '$hook', "PLHooks::run_action($hook);" ), $data['priority'] );
 						}
 				} else {
-					add_action( $data['hook'], create_function( '$hook', "PLHooks::run_action($hook);"), $data['priority'] );
-				}	
-			}				
+					add_action( $data['hook'], create_function( '$hook', "PLHooks::run_action($hook);" ), $data['priority'] );
+				}
+			}
 		}
 	}
 
 	function run_action( $hook_id ) {
-			
+
 		global $options;
 		if( ! is_array( $options ) )
 			$options = array();
@@ -90,7 +90,7 @@ class PLHooks {
 			global $hook_contents;
 			global $hook_id;
 			$hook_id = $option['hook_id'];
- 
+
 			$php = stripslashes( $option['content'] );
 
 			$hook_name = $hook;
@@ -113,11 +113,11 @@ class PLHooks {
 
 		if( isset( $_POST['import-hooks'] ) ) {
 
-			$hooks = pl_file_get_contents($_FILES['file']['tmp_name']);
+			$hooks = pl_file_get_contents( $_FILES['file']['tmp_name'] );
 			$hooks = json_decode( $hooks );
 			if ( is_object( $hooks ) ) {
-				update_option( 'pl_hooks_editor', json_decode(json_encode($hooks), true) );
-				wp_redirect( admin_url( 'admin.php?page=pagelines_hooks&imported=true' ) ); 
+				update_option( 'pl_hooks_editor', json_decode( json_encode( $hooks ), true ) );
+				wp_redirect( admin_url( 'admin.php?page=pagelines_hooks&imported=true' ) );
 			}
 		}
 
@@ -126,14 +126,14 @@ class PLHooks {
 			$options = get_option( 'pl_hooks_editor', array() );
 
 			if ( isset($options) && is_array( $options) ) {
-				
-				header('Cache-Control: public, must-revalidate');
-				header('Pragma: hack');
-				header('Content-Type: text/plain');
-				header( 'Content-Disposition: attachment; filename="hookers.pimp"' );						
+
+				header( 'Cache-Control: public, must-revalidate' );
+				header( 'Pragma: hack' );
+				header( 'Content-Type: text/plain' );
+				header( 'Content-Disposition: attachment; filename="hookers.pimp"' );
 				echo json_encode( $options );
 				exit();
-			} 
+			}
 		}
 
 		if( isset( $_POST['hooks-delete'] ) && '' != $_POST['hook'] ) {
@@ -163,7 +163,7 @@ class PLHooks {
 
 			if( isset( $_POST['php'] ) )
 				$options[$id]['php'] = true;
-			else 
+			else
 				$options[$id]['php'] = false;
 
 			$options[$id]['error'] = false;
@@ -185,8 +185,8 @@ class PLHooks {
 	function get_admin_page() {
 
 		$args = array(
-			'title'			=> 'Hook Editor', 
-			'show_save'		=> false, 
+			'title'			=> __( 'Hook Editor', 'hooker' ),
+			'show_save'		=> false,
 			'show_reset'	=> false,
 			'callback'		=> array( &$this, 'setup_admin_page' ),
 		);
@@ -226,12 +226,12 @@ class PLHooks {
 		global $options;
 		$options = get_option( 'pl_hooks_editor', array() );
 		echo'<p><select id="hooks" name="hooks">';
-		echo "<option name='none' id='none' value='none'>Add a new hook...</option>";
-		echo '<option disabled="disabled">WordPress Hooks</option>';
+		printf( "<option name='none' id='none' value='none'>%s</option>", __( 'Add a new hook...', 'hooker' ) );
+		printf( '<option disabled="disabled">%s</option>', __( 'WordPress Hooks', 'hooker' ) );
 		foreach ( $wp_hooks as $k => $hook ) {
 			echo "<option name='{$hook}' id='hook-{$hook}' value='{$hook}'>{$hook}</option>";
 		}
-		echo '<option disabled="disabled">PageLines Hooks</option>';
+		printf( '<option disabled="disabled">%s</option>', __( 'PageLines Hooks', 'hooker' ) );
 
 		$hooks = json_decode( $this->get_pl_hooks() );
 
@@ -242,7 +242,7 @@ class PLHooks {
 
 			echo "<option name='{$hook}' id='hook-{$hook}' value='{$hook}'>{$hook}</option>";
 		}
-		echo '<option disabled="disabled">PageLines Sections</option>';
+		printf( '<option disabled="disabled">%s</option>', __( 'PageLines Sections', 'hooker' ) );
 		$sections = $this->get_section_hooks();
 		foreach ( $sections as $o => $hook ) {
 
@@ -260,12 +260,12 @@ class PLHooks {
 		}
 		if( ! empty( $options ) ) {
 			echo'<p><select id="hooks_active" name="hooks_active">';
-			echo "<option name='none' id='none' value='none'>Edit hook...</option>";
+			printf( "<option name='none' id='none' value='none'>%s</option>", __( 'Edit hook...', 'hooker' ) );
 
 			foreach ( $options as $o => $hook ) {
 				$error = '';
 				if( isset( $hook['error'] ) && '' != $hook['error'] )
-					$error = '[! error !] ';
+					$error = sprintf( '[! %s !] ', __( 'Error', 'hooker' ) );
 
 				if( isset( $hook['content'] ) && '' != $hook['content'] )
 					echo "<option name='{$o}' id='hook-{$o}' value='{$o}'>{$error}{$hook['hook']}</option>";
@@ -303,11 +303,11 @@ class PLHooks {
 
 		global $load_sections;
 		$available = $load_sections->pagelines_register_sections( false, true );
-	
-		$sections = array();	
-		foreach( $available as $type ) {	
+
+		$sections = array();
+		foreach( $available as $type ) {
 			foreach( $type as $key => $data ) {
-			
+
 				$sections[] = sprintf( 'pagelines_before_%s', basename( $data['base_dir'] ) );
 				$sections[] = sprintf( 'pagelines_inside_bottom_%s', basename( $data['base_dir'] ) );
 				$sections[] = sprintf( 'pagelines_after_%s', basename( $data['base_dir'] ) );
@@ -339,7 +339,7 @@ class PLHooks {
 			if ( ! isset( $options[ $hook ] ) )
 				$options[ $hook ] = $defaults;
 		}
-		
+
 		// draw option boxes....
 
 		foreach( $options as $o => $d ) {
@@ -354,27 +354,37 @@ class PLHooks {
 				$check = 'checked';
 			else
 				$check = 'unchecked';
-			echo "<br /><input type='checkbox' name='enabled' " . $check . " /> Enable this hook.";			
+			printf( "<br /><input type='checkbox' name='enabled' %s /> %s", $check, __( 'Enable this hook.', 'hooker' ) );
 
 			if ( defined( 'PL_HOOKS_PHP' ) ) {
 
 				if( isset( $d['error'] ) && false != $d['error'] )
-					echo "<p><span style='color:red' >This hook generated a PHP Fatal Error the last time it was executed.</span><br >The error reported was: <strong>{$d['error']}</strong></p>";
+					printf( "<p><span style='color:red' >%s</span><br >%s <strong>%s</strong></p>",
+					__( 'This hook generated a PHP Fatal Error the last time it was executed.', 'hooker' ),
+					__( 'The error reported was:', 'hooker' ),
+					$d['error']
+					);
 
 				if( isset( $d['php'] ) && true === $d['php'] )
 					$check = 'checked';
 				else
 					$check = 'unchecked';
-				echo "<br /><input type='checkbox' name='php' " . $check . " /> Enable PHP for this hook.";			
+				printf( "<br /><input type='checkbox' name='php' %s /> %s.",
+					$check,
+					__( 'Enable PHP for this hook', 'hooker' )
+					);
 			}
-			echo "<br /><input  name='priority' value='{$d['priority']}' /> Priority. (default is 10)";
-			printf( '<br /><input name="page_id" value="%s" /> Only show on these pages/posts (coma seperated list of IDs).', implode( ',', $d['page_id'] ) );
-			echo '<p><input type="submit" name="hooks-post" value="Save Hook" class="button-primary" />';
+			printf( "<br /><input  name='priority' value='{$d['priority']}' /> %s", __( 'Priority. (default is 10)', 'hooker' ) );
+			printf( '<br /><input name="page_id" value="%s" /> %s.',
+				implode( ',', $d['page_id'] ),
+				__( 'Only show on these pages/posts (coma seperated list of IDs)', 'hooker' )
+				);
+			printf( '<p><input type="submit" name="hooks-post" value="%s" class="button-primary" />', __( 'Save Hook', 'hooker' ) );
 
 			if( '' != $d['content'] ) {
 
 				$h = str_replace( '-', '_', $d['hook'] );
-				echo "<input type='submit' name='hooks-delete' value='Delete Hook' class='button-primary' onClick='return ConfirmRestore();'/>";
+				printf( "<input type='submit' name='hooks-delete' value='%s' class='button-primary' onClick='return ConfirmRestore();'/>", __( 'Delete Hook', 'hooker' ) );
 				pl_action_confirm( 'ConfirmRestore', "Are you sure??\n\nThis will delete this hook.\n\nDont be complaining after you have deleted it!!" );
 			}
 			if ( isset( $d['hook'] ) )
@@ -423,8 +433,8 @@ echo '</div></form>';
 
 		echo '<div><p>';
 
-				echo "<input type='submit' name='export-hooks' value='Export Hooks' class='button-primary' /> Export your hooks to a file.</form>";
-	
+				printf( "<input type='submit' name='export-hooks' value='Export Hooks' class='button-primary' /> %s.</form>", __( 'Export your hooks to a file', 'hooker' ) );
+
 				echo "<form enctype='multipart/form-data' action='' method='post'><input type='submit' name='import-hooks' value='Import Hooks' class='button-primary' onClick='return ConfirmImport();' />";
 				echo '<input type="file" class="file_uploader text_input" name="file" id="hooks-file" />';
 
@@ -491,8 +501,8 @@ jQuery(document).ready(function() {
 } // end PLHooks class
 
 add_action('admin_menu', 'setup_pl_hooker', 15);
-function setup_pl_hooker() {	
-		
+function setup_pl_hooker() {
+
 	new PLHooks;
 }
 
@@ -507,16 +517,16 @@ function fatal_error_handler_hooks($buffer){
     if($error['type'] == 1){
         // type, message, file, line
         $newBuffer='<html><header><title>Error</title></header>
-                    <style>                 
-                    .error_content{                     
+                    <style>
+                    .error_content{
                         background: ghostwhite;
                         vertical-align: middle;
                         margin:0 auto;
                         padding:10px;
-                        width:50%;                              
-                     } 
+                        width:50%;
+                     }
                      .error_content label{color: red;font-family: Georgia;font-size: 16pt;font-style: italic;}
-                     .error_content ul li{ background: none repeat scroll 0 0 FloralWhite;                   
+                     .error_content ul li{ background: none repeat scroll 0 0 FloralWhite;
                                 border: 1px solid AliceBlue;
                                 display: block;
                                 font-family: monospace;
@@ -524,19 +534,19 @@ function fatal_error_handler_hooks($buffer){
                                 text-align: left;
                       }
                     </style>
-                    <body style="text-align: center;">  
+                    <body style="text-align: center;">
                       <div class="error_content">
                           <label>An action has resulted in a Fatal error!</label>
                           <ul>
                           	<li><b>Hook: </b>' . $hook_name . '</li>
-                            <li><b>Message: </b> '.$error['message'].'</li>                      
+                            <li><b>Message: </b> '.$error['message'].'</li>
                           </ul>
                           <textarea>' . $hook_contents .' </textarea>
 
                           <br />
                           <p>
                           <strong>PHP execution for this hook has now been disabled.</strong></p>
-			                      
+
                       </div>
                     </body></html>';
 
